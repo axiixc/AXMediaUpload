043185e6587ba96fe6a54be1b04aeb8b0232564b
James Savage <axiixc@gmail.com>
2013-04-16 17:20:34 -0400

    Base demo commit

---
+++ AXMediaUploadController/AXAppDemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  AXAppDemoViewController.h
+//  AXMediaUploadController
+//
+//  Created by James Savage on 4/16/13.
+//  Copyright (c) 2013 James Savage <me@axiixc.com>. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AXAppDemoViewController : UIViewController
+
+@end
---
+++ AXMediaUploadController/AXAppDemoViewController.m
@@ -0,0 +1,35 @@
+//
+//  AXAppDemoViewController.m
+//  AXMediaUploadController
+//
+//  Created by James Savage on 4/16/13.
+//  Copyright (c) 2013 James Savage <me@axiixc.com>. All rights reserved.
+//
+
+#import "AXAppDemoViewController.h"
+
+@interface AXAppDemoViewController ()
+
+@property (nonatomic, strong) IBOutlet UIImageView * imageView;
+@property (nonatomic, strong) IBOutlet UIToolbar * mediaSelectBar;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem * mediaSelectButtonItem;
+@property (nonatomic, strong) IBOutlet UILabel * mediaUploadLabel;
+@property (nonatomic, strong) IBOutlet UIProgressView * mediaUploadProgressView;
+
+@end
+
+@implementation AXAppDemoViewController
+
+- (id)init
+{
+    return [super initWithNibName:NSStringFromClass([AXAppDemoViewController class]) bundle:nil];
+}
+
+#pragma mark - Controller Workflow
+
+- (IBAction)selectMedia:(id)sender
+{
+    
+}
+
+@end
