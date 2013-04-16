5803424d825c013a39456add9ff24399f5a0614f
James Savage <axiixc@gmail.com>
2013-04-16 17:03:51 -0400

    Initial Commit

---
+++ AXMediaUploadController/AXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AXAppDelegate.h
+//  AXMediaUploadController
+//
+//  Created by James Savage on 4/16/13.
+//  Copyright (c) 2013 James Savage <me@axiixc.com>. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AXMediaUploadController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AXMediaUploadController
+//
+//  Created by James Savage on 4/16/13.
+//  Copyright (c) 2013 James Savage <me@axiixc.com>. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AXAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AXAppDelegate class]));
+    }
+}
